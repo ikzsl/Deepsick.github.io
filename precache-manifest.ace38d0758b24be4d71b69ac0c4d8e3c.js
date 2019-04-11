c16724afa6b33a849595dc45dbd247ad6362bb3c
deepsick <mav96@bk.ru>
2019-04-11 16:30:43 +0300

    adds build

---
+++ precache-manifest.ace38d0758b24be4d71b69ac0c4d8e3c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d81df4d4edb734d9713d8ffbd4374204",
+    "url": "/static/media/previous-page-icon.d81df4d4.svg"
+  },
+  {
+    "revision": "b767ab034ea02ad3322e6073eab05426",
+    "url": "/static/media/first-page-icon.b767ab03.svg"
+  },
+  {
+    "revision": "6b56b616ee9420fbb9fe38733d3d8a48",
+    "url": "/static/media/back-button-hover.6b56b616.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "91fad4994e5d9d620f5d",
+    "url": "/static/js/main.80cd4441.chunk.js"
+  },
+  {
+    "revision": "37aaf4dba33034378e57",
+    "url": "/static/js/2.0003a2db.chunk.js"
+  },
+  {
+    "revision": "91fad4994e5d9d620f5d",
+    "url": "/static/css/main.8d387114.chunk.css"
+  },
+  {
+    "revision": "c479d50e09370f0d5c85221e7c887873",
+    "url": "/index.html"
+  }
+];
